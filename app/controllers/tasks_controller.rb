529d77784aed0a9559b2ebb0054c965f2e758254
Wendy Harris <wendyv510@hotmail.com>
2021-10-12 11:15:09 -0400

    task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
 class TasksController < ApplicationController
+
+    def index 
+        @tasks = Task.all 
+        render json: @tasks 
+    end 
+
+    def show
+        render json: @tasks 
+    end 
+
+    def create
+        @task = Task.new(task_params) 
+    end 
+
+    def update 
+    end 
+
+    def destroy 
+        @task.destroy
+    end 
+
+    private 
+
+    def task_params
+        params.require(:task).permit(:text, :day, :reminder) 
+    end 
 end
